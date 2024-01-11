1a696a5ad7b4363c5fcdc95dfe915e705e00b10d
cbsezgin <cbsezgin@hotmail.com>
2024-01-11 19:06:24 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from routers import blog_get, blog_post
+from db.models import Base
+from db.database import engine
+
+app = FastAPI()
+app.include_router(blog_get.router)
+app.include_router(blog_post.router)
+
+
+@app.get('/')
+def index():
+    return {"message": "Hello world!"}
+
+
+Base.metadata.create_all(engine)
